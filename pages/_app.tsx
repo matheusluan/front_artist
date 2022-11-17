f2eeef4d76c6f707a69d0c005b937670436be944
Matheus <matheusbrito1999@live.com>
2022-11-17 19:00:04 -0300

    first commit - front artist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CssBaseline } from '@mui/material'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+       <Head><title>Artist</title></Head>
+       <CssBaseline />
+       <Component {...pageProps} />
+    </>
+    )
 }
