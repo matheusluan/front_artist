e16bd3dab075aca94529724cf58e15293c504023
Matheus <matheusbrito1999@live.com>
2022-11-19 10:50:25 -0300

    ajuste meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,12 @@ import { CssBaseline } from '@mui/material'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-       <Head><title>Artist</title></Head>
-       <CssBaseline />
-       <Component {...pageProps} />
+      <Head>
+        <title>Artist</title> 
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
     </>
-    )
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -68,11 +68,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,500;0,600;1,900&display=swap" rel="stylesheet" />
           <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
             integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-            crossOrigin="" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
+            crossOrigin="" />         
         </Head>
         <body>
           <Main />
