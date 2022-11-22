c0192a00f10462a5162407c610e9674daeeccde6
Matheus <matheusbrito1999@live.com>
2022-11-21 22:36:52 -0300

    vai toma no cu

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   const Map = useMemo(() => dynamic(() => import('./components/Map'), {
     loading: () => <p>A map is loading</p>,
-    ssr: false,
+    ssr: true,
 }), []);
 
  const isMounted = RenderCompleted();
